2077f0d42864ffa94372fac0f844688ca282cd04
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-02-11 02:14:13 +0900

    2022.02.11

---
+++ JS_SU/main2.js
@@ -0,0 +1,34 @@
+let myname = '드림보이';
+alert(myname);
+console.log(myname);
+document.write(myname+"<br>");
+
+let myString = '배';
+let currentDate = new Date();
+let myFunction = () => console.log('myFunction');
+
+let myname2 = '사과';
+myname2 = '바보';
+alert(myname2);
+console.log(myname2);
+document.write(myname2+"<br>");
+
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+alert(sum);
+console.log(sum);
+document.write(sum+"<br>");
+
+let firstName = '바보';
+let familyName = '천재';
+let fullName = firstName + familyName;
+alert(fullName);
+console.log(fullName);
+document.write(fullName+"<br>");
+
+let value1 = 100;
+let value2 = value1;
+alert(value2);
+console.log(value2);
+document.write(value2+"<br>");
---
+++ JS_SU/main3.js
@@ -0,0 +1,27 @@
+const MyName = '사자';
+alert(MyName);
+console.log(MyName);
+document.write(MyName+"<br>");
+
+const MyString = '사자';
+const MyFunction = () => console.log('함수 확인');
+
+const MyName2 = '자바';
+MyName3 = '스크립트';
+alert(MyName2);
+console.log(MyName2);
+document.write(MyName2+"<br>");
+
+const number1 = 10;
+const number2 = 20;
+const sum = number1 + number2;
+alert(sum);
+console.log(sum);
+document.write(sum+"<br>");
+
+const familyName = '테스트';
+const firstName = '입니다';
+const fullName = familyName + firstName;
+alert(fullName);
+console.log(fullName);
+document.write(fullName+"<br>");
